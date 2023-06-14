fe38af7e3859c3e7e31ca98cdc2146ee9d26a936
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2023-06-13 21:01:01 -0700

    setup color pallete variables for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className=" bg-slate-500 min-h-screen">
+      <div className="  bg-background text-text min-h-screen">
         <h1 className="text-4xl text-center text-white">Hello World</h1>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      text: "#050505",
+      background: "#fafafa",
+      secondaryButton: "#ffffff",
+      accent: "#676864"
+    },
   },
   plugins: [],
-}
-
+};
